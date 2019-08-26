5230156565ddbe7771c203e9524c1cae4d9a0ab0
Musa Ugurlu <musaugurlu@outlook.com>
2019-08-25 23:18:21 -0400

    Initial Commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Sign In - InstaShopMatic</title>
+  <title>Sign In - {{ config('app.name', 'New App') }}</title>
   <style>
     #loader {
       transition: all .3s ease-in-out;
